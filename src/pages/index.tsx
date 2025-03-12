c6b173c193e35b43eb6ad60d21c7a3b0f6a69a3a
benjiaming <benjiaming@users.noreply.github.com>
2025-03-12 13:20:09 -0700

    ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import dynamic from 'next/dynamic'
  
-const PublicLeadForm = dynamic(() => import('../components/PublicLeadForm'), { ssr: false })
+const PublicLeadForm = dynamic(() => import('../components/PublicLeadForm'), { ssr: true })
  
 const Home = () => {
     return (
